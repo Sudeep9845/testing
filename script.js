730e779ab9e60d1278224c6d3704bde25ed46487
Sudeep9845 <119121660+Sudeep9845@users.noreply.github.com>
2023-06-07 01:43:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20)
+        {
+            $('.navbar').addClass("sticky");
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+    //toogle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass('active');
+    });
+});
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+
+
+    // // toggle menu/navbar script
+    // $('.menu-btn').click(function(){
+    //     $('.navbar .menu').toggleClass("active");
+    //     $('.menu-btn i').toggleClass("active");
+    // });
